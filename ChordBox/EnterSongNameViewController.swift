6a742d7202b838e2a4d2d4fc267c5bf6c19a0732
Minho-Choi <2153373@naver.com>
2021-02-18 11:46:52 +0900

    lyrics search api implemented

--- ChordBox/EnterSongNameViewController.swift
+++ ChordBox/EnterSongNameViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class EnterSongNameViewController: UIViewController {
+class EnterSongNameViewController: UIViewController, UITextFieldDelegate {
+    
+
+    @IBOutlet weak var songTitleOutlet: UITextField!
+    @IBOutlet weak var artistNameOutlet: UITextField!
+    var lyrics: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +20,44 @@ class EnterSongNameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func fetchLyricsDataFromAPI(title: String, artist: String) {
+        let url = URL(string: "https://api.lyrics.ovh/v1/\(artist)/\(title)")
+        let session = URLSession.shared
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let validURL = url {
+                let dataTask = session.dataTask(with: validURL) { (data, response, error) in
+                    guard error == nil else {
+                        print("Error occur: \(String(describing: error))")
+                        return
+                    }
+                    guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                        print("Not Found")
+                        return
+                    }
+                    do {
+                        let lyricsJSON = try JSONDecoder().decode(LyricsJSON.self, from: data)
+                        self.lyrics = lyricsJSON.lyrics
+                        print("lyrics: \(self.lyrics)")
+                    } catch {
+                        print("JSON Parsing Error")
+                        print(error.localizedDescription)
+                    }
+                }
+                dataTask.resume()
+            } else {
+                print("invalid url : \(String(describing: url?.absoluteString))")
+            }
+            }
+        }
+        
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        print("button pressed")
+        if let title = songTitleOutlet.text, let artist = artistNameOutlet.text {
+            fetchLyricsDataFromAPI(title: title.replacingOccurrences(of: " ", with: "%20"), artist: artist)
+        }
+    }
+}
 
+struct LyricsJSON : Codable {
+    var lyrics: String
 }
